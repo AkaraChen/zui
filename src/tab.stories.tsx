d371bbb5c40302001c97f67ace3362af52717dfe
无尘室内点墨 <akarachen@outlook.com>
2024-08-23 20:54:42 +0800

    feat: vertical tab

--- src/tab.stories.tsx
+++ src/tab.stories.tsx
@@ -1,42 +1,79 @@
 import {
     Code2Icon,
+    ListTreeIcon,
+    SpellCheck2Icon,
     SquareFunctionIcon,
     TerminalIcon,
     TextCursorIcon,
     TreePineIcon,
+    WrapTextIcon,
 } from 'lucide-react'
 import { useState, type FC } from 'react'
 import { Tab, type TabItem } from './tab'
 
-const tabs: TabItem[] = [
-    {
-        id: 'muilti-model',
-        label: 'Muilti Model',
-        icon: Code2Icon,
-    },
-    {
-        id: 'interactive-programming',
-        label: 'Interactive Programming',
-        icon: SquareFunctionIcon,
-    },
-    {
-        id: 'multi-language',
-        label: 'Multi Language',
-        icon: TreePineIcon,
-    },
-    {
-        id: 'terminal-task-manager',
-        label: 'Terminal Task Manager',
-        icon: TerminalIcon,
-    },
-    {
-        id: 'vim-friendly',
-        label: 'Vim Friendly',
-        icon: TextCursorIcon,
-    },
-]
-
 export const normal: FC = () => {
+    const tabs: TabItem[] = [
+        {
+            id: 'muilti-model',
+            label: 'Muilti Model',
+            icon: Code2Icon,
+        },
+        {
+            id: 'interactive-programming',
+            label: 'Interactive Programming',
+            icon: SquareFunctionIcon,
+        },
+        {
+            id: 'multi-language',
+            label: 'Multi Language',
+            icon: TreePineIcon,
+        },
+        {
+            id: 'terminal-task-manager',
+            label: 'Terminal Task Manager',
+            icon: TerminalIcon,
+        },
+        {
+            id: 'vim-friendly',
+            label: 'Vim Friendly',
+            icon: TextCursorIcon,
+        },
+    ]
     const [tab, setTabs] = useState<TabItem>(tabs[0]!)
     return <Tab tabs={tabs} tab={tab} onTabChange={setTabs} />
+}
+
+export const vertical: FC = () => {
+    const tabs: TabItem[] = [
+        {
+            id: 'language-server-support',
+            label: 'Language Server support',
+            description: 'Integrate with any language server.',
+            icon: SpellCheck2Icon,
+        },
+        {
+            id: 'outline-view',
+            label: 'Outline View',
+            description: "Navigate through a file's symbols.",
+            icon: ListTreeIcon,
+        },
+        {
+            id: 'text-and-line-maniplation',
+            label: 'Text and Line Manipulation',
+            description: 'Smoothly and efficiently edit your code.',
+            icon: WrapTextIcon,
+        },
+    ]
+    const [tab, setTabs] = useState<TabItem>(tabs[0]!)
+    return (
+        <Tab
+            tabs={tabs}
+            tab={tab}
+            onTabChange={setTabs}
+            vertical
+            style={{
+                width: '512px',
+            }}
+        />
+    )
 }
--- src/tab.tsx
+++ src/tab.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
 import type { LucideIcon } from 'lucide-react'
-import type { FC, ReactNode } from 'react'
+import type { ComponentProps, FC, ReactNode } from 'react'
 import { Icon } from './icon'
 import './tab.less'
 import { zc } from './utils'
@@ -9,31 +9,68 @@ export interface TabItem {
     id: string
     label: ReactNode
     icon: LucideIcon
+    description?: string
 }
 
-export interface TabProps {
+export interface TabProps extends ComponentProps<'div'> {
     tabs: TabItem[]
     tab: TabItem
     onTabChange: (tab: TabItem) => void
+    vertical?: boolean
 }
 
 export const Tab: FC<TabProps> = props => {
-    const { tabs, tab, onTabChange } = props
+    const { tabs, tab, onTabChange, className, vertical, ...rest } = props
     return (
-        <div className={zc('tab')}>
-            {tabs.map(t => (
-                <div
-                    key={t.id}
-                    className={clsx(
-                        zc('tab', 'item'),
-                        tab.id === t.id && zc('tab', 'item', 'active'),
-                    )}
-                    onClick={() => onTabChange(t)}
-                >
-                    <Icon lucide={t.icon} />
-                    {t.label}
-                </div>
-            ))}
+        <div
+            className={clsx(
+                className,
+                zc('tab'),
+                vertical && zc('tab', 'vertical'),
+            )}
+            {...rest}
+        >
+            {tabs.map(t =>
+                !vertical ? (
+                    <div
+                        key={t.id}
+                        className={clsx(
+                            zc('tab', 'item'),
+                            tab.id === t.id && zc('tab', 'item', 'active'),
+                        )}
+                        onClick={() => onTabChange(t)}
+                    >
+                        <Icon lucide={t.icon} />
+                        {t.label}
+                    </div>
+                ) : (
+                    <div
+                        key={t.id}
+                        className={clsx(
+                            zc('tab', 'item'),
+                            zc('tab', 'item', 'vertical'),
+                            tab.id === t.id && zc('tab', 'item', 'active'),
+                        )}
+                        onClick={() => onTabChange(t)}
+                    >
+                        <div
+                            className={clsx(
+                                zc('tab', 'item', 'vertical', 'head'),
+                            )}
+                        >
+                            <Icon lucide={t.icon} />
+                            {t.label}
+                        </div>
+                        <div
+                            className={clsx(
+                                zc('tab', 'item', 'vertical', 'description'),
+                            )}
+                        >
+                            {t.description}
+                        </div>
+                    </div>
+                ),
+            )}
         </div>
     )
 }
