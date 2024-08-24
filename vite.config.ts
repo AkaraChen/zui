e40abf6c1bdda0748ad3287f5ba92f842e3afe62
无尘室内点墨 <akarachen@outlook.com>
2024-08-24 16:08:31 +0800

    feat: ready for publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import pkg from './package.json'
+import dts from 'vite-plugin-dts'
 
-export default defineConfig({
+const dev = defineConfig({
     plugins: [react()],
 })
+
+const prod = defineConfig({
+    plugins: [react(), dts()],
+    build: {
+        lib: {
+            entry: './src/index.ts',
+            formats: ['es'],
+        },
+        minify: false,
+        rollupOptions: {
+            external: [
+                ...Object.keys(pkg.dependencies || {}),
+                new RegExp('react'),
+            ],
+        },
+    },
+})
+
+export default process.env.NODE_ENV === 'production' ? prod : dev
